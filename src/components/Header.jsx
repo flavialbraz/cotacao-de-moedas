630400668b20fc7d6d13ab1d5da0ccae7b6886e9
Flávia.AL.Braz <78583429+flavialbraz@users.noreply.github.com>
2023-01-27 23:00:45 -0300

    Paragrafo da header atualizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header () {
           <div className="money"> </div>
           <h1> Cotação </h1>
           <p>
-            Organize as suas finanças e viaje com tranquilidade! Converta valores em REAL para a moeda estrangeira escolhida.
+            Organize as suas finanças e viaje com tranquilidade! Converta valores em REAL antes de comprar a moeda estrangeira escolhida.
             {/* Acompanhe a <strong> cotação das moedas mais importantes do mundo </strong> em
             tempo real e organize as suas finanças para a próxima viagem.  */}
           </p>
